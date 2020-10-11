f3a048894e0f0f26317cde5ea2d0d0bf2f5ccb84
Bob Feuerstein <thebobbyllama@yahoo.com>
2020-10-11 16:50:35 -0500

    Updates

---
+++ precache-manifest.4d23ebf6b3f9be1aae205bddad73b4e1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d6f5dcca356bb10f6108d19e66fc2c",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "002fba9f707a70edb9d5",
+    "url": "/portfolio-react/static/css/main.26435c75.chunk.css"
+  },
+  {
+    "revision": "778db59167f4b433d79c",
+    "url": "/portfolio-react/static/js/2.13de9edb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-react/static/js/2.13de9edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "002fba9f707a70edb9d5",
+    "url": "/portfolio-react/static/js/main.366be489.chunk.js"
+  },
+  {
+    "revision": "2f26d0aed242c262185b",
+    "url": "/portfolio-react/static/js/runtime-main.a4de92d3.js"
+  },
+  {
+    "revision": "4907a8184d3ec1d5f27c0406741fdc53",
+    "url": "/portfolio-react/static/media/Resume - Bob Feuerstein.4907a818.pdf"
+  },
+  {
+    "revision": "2cd12fe7f9e21527a229875f800ac8f0",
+    "url": "/portfolio-react/static/media/banner.2cd12fe7.jpg"
+  },
+  {
+    "revision": "a6f42990b4bb77fb213b3e41cd140d35",
+    "url": "/portfolio-react/static/media/bfeuerstein.a6f42990.jpg"
+  },
+  {
+    "revision": "d617a6143bdf7e9c63d70cc70796e424",
+    "url": "/portfolio-react/static/media/skyrim-RMAO.d617a614.gif"
+  },
+  {
+    "revision": "927342e8cfd9a4735cddfe1005a9366b",
+    "url": "/portfolio-react/static/media/skyrim-dwemermatron.927342e8.jpg"
+  },
+  {
+    "revision": "3bddcc82267eaecdb63f8c091bb565b9",
+    "url": "/portfolio-react/static/media/skyrim-reincarnation.3bddcc82.jpg"
+  }
+]);
